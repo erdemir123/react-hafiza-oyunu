50260488557f7f42ecc3071b9d337ac67f43a4ed
erdemir123 <sadikerdemir93@gmail.com>
2022-12-26 21:46:48 +0300

    app designed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import "./App.css";
 import Card from "./Components/Card";
 
 const kartResimler = [
-  { src: "/img/bjk.png" },
-  { src: "/img/gs.png" },
-  { src: "/img/çorum.png" },
-  { src: "/img/chelsea.png" },
-  { src: "/img/fb.png" },
-  { src: "/img/ts.png" },
+  { src: "/img/bjk.png", eşleşme: false },
+  { src: "/img/gs.png", eşleşme: false },
+  { src: "/img/çorum.png", eşleşme: false },
+  { src: "/img/chelsea.png", eşleşme: false },
+  { src: "/img/fb.png", eşleşme: false },
+  { src: "/img/ts.png", eşleşme: false },
 ];
 function App() {
   const [kartlar, setKartlar] = useState([]);
   const [ilk, setilk] = useState();
   const [iki, setiki] = useState();
   const [sayı, setsayı] = useState(0);
+  const [oyunbitti, setOyunbitti] = useState();
   const karıştır = () => {
     const karıştırılmışKartlar = [...kartResimler, ...kartResimler]
       .sort(() => Math.random() - 0.5)
@@ -23,37 +24,62 @@ function App() {
   };
   const kartSeç = (kart) => {
     ilk ? setiki(kart) : setilk(kart);
+    console.log(ilk,iki);
   };
+  const yeniOyun=()=>{
+    setOyunbitti(!oyunbitti)
+  }
 
   useEffect(() => {
     if (ilk && iki) {
       if (ilk.src === iki.src) {
-        console.log("kartlar eşleştit");
         setiki();
         setilk();
-        setsayı(öncekisayı=>öncekisayı+1)
-      }
-      else{
-        console.log("kartlar eşleşmedi");
+        setsayı((öncekisayı) => öncekisayı + 1);
+        setKartlar((öncekiKart) => {
+          return öncekiKart.map((kart) => {
+            if (kart.src === ilk.src) {
+              return { ...kart, eşleşme: true };
+
+            } else {
+              return kart;
+            }
+          });
+        });
+      } else {
         setiki();
-        setilk()
-        setsayı(öncekisayı=>öncekisayı+1)
+        setilk();
+        setsayı((öncekisayı) => öncekisayı + 1);
       }
     }
-    console.log(sayı)
+   setOyunbitti(kartlar.every((kart)=>(kart.eşleşme)))
+   
   }, [ilk, iki]);
-
-  return (
-    <div className="App">
-      <h1>React Hafıza Oyunu</h1>
-      <button onClick={karıştır}>Yeni Oyun</button>
-      <div className="card-grid">
-        {kartlar.map((kart) => (
-          <Card kart={kart} kartSeç={kartSeç} />
-        ))}
+  if(oyunbitti){
+    return (
+      <div>
+      <div>tebrikler</div>
+      <button onClick={yeniOyun}>Yeni Oyun</button>
       </div>
-    </div>
-  );
+      
+    )
+  }else{
+    return (
+      <div className="App">
+        <h1>React Hafıza Oyunu</h1>
+        <button onClick={karıştır}>Yeni Oyun</button>
+        <div className="card-grid">
+          {kartlar.map((kart) => (
+            <Card kart={kart} kartSeç={kartSeç} dönüş={kart === ilk || kart === iki || kart.eşleşme} />
+          ))}
+        </div>
+        <div>
+          <p>{sayı}</p>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,10 @@
 import "./Card.css"
 import React from 'react'
 
-const Card = ({kart,kartSeç}) => {
+const Card = ({kart,kartSeç,dönüş}) => {
   return (
     <div className="card" key={kart.id}>
-            <div >
+            <div className={dönüş ? "flipped":""}>
               <img src={kart.src} alt="" className="front" />
               <img src="/img/kapak.png" alt="" className="back" onClick={()=>kartSeç(kart)}/>
             </div>
